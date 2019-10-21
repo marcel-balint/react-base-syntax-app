76fe510b639b28e124bb9c33657d7a2be6ec9f0e
Marcel Balint <blank_blankness@yahoo.com>
2019-10-21 10:24:19 -0400

    Added a function to remove a list item

--- src/App.js
+++ src/App.js
@@ -32,6 +32,13 @@ class App extends Component {
     });
   };
 
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
@@ -49,8 +56,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+              />
+            );
           })}
         </div>
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,10 @@ import "./Person.css";
 const Person = props => {
   return (
     <div className="Person">
-      <p onClick={props.click}>
+      <span className="Close" onClick={props.click}>
+        &#10006;
+      </span>
+      <p>
         I'm {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>
